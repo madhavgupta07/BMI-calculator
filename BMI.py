7392859b6f9c8bcb85da534e48cfbff6e85d22b3
Madhav Gupta <72290706+madhavgupta07@users.noreply.github.com>
2020-10-26 13:26:53 +0530

    Add files via upload

---
+++ BMI.py
@@ -0,0 +1,7 @@
+"""This is a BMI calculator"""
+print("This is a BMI calculator")
+name=str(input("Enter your name: "))
+mass=int(input("Enter Your Weight in kg's: "))
+height=float(input("Enter your weight in meters: "))
+BMI=mass/(height*height)
+print('Hey {name}, your height is {height} and your BMI is {BMI} ')
